1ef5b1f8bfd03a94a3a155e7983edaf2216352c7
fxxdfp00976wwnemm <vanaal.artdepartment@gmail.com>
2024-01-31 11:51:02 -0500

    Primera subida

--- js/scroll_index.js
+++ js/scroll_index.js
@@ -1,120 +1,91 @@
 window.sr = ScrollReveal();
-    //Index
-  /*  sr.reveal('.contenedor_slider_index h2', {
-        duration: 2000,
-        origin: 'right',
-        distance: '-100px'
-    });
+/*Inicio*/
+/*Slider editorial */
+sr.reveal('.navslider', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
 
-    sr.reveal('.contenedor_slider_index p', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });
+sr.reveal('.navslider h2', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-100px'
+});
 
-    sr.reveal('.contenedor_slider_index #boton_slider', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });*/
+sr.reveal('.navslider p', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
 
+sr.reveal('.navslider1', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
 
-    sr.reveal('#textobanner h2', {
-        duration: 2000,
-        origin: 'bottom',
-        distance: '-100px'
-    });
+sr.reveal('.navslider1 h2', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-100px'
+});
 
-    sr.reveal('#textobanner p', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });
+sr.reveal('.navslider1 p', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
 
-    sr.reveal('.contenedorproducto', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });
+/*Producto*/
+sr.reveal('.contenedorproducto', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
 
-    sr.reveal('.contenedorproducto .textoproducto h2', {
-        duration: 2000,
-        origin: 'right',
-        distance: '-100px'
-    });
+sr.reveal('.contenedorproducto .textoproducto h2', {
+    duration: 2000,
+    origin: 'right',
+    distance: '-100px'
+});
 
-    sr.reveal('.contenedorproducto .textoproducto p', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });
+sr.reveal('.contenedorproducto .textoproducto p', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
 
-    sr.reveal('.contenedorproducto .textoproducto .botonproducto', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });
+sr.reveal('.contenedorproducto .textoproducto .botonproducto', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
 
-    sr.reveal('.contenedorproducto1', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });
+/*New Works */
+sr.reveal('.newworks', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
 
-    sr.reveal('.navslider', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });
+sr.reveal('footer', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
 
+//About
+sr.reveal('.contenedorabout', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '-100px'
+});
 
-    sr.reveal('.navslider h2', {
-        duration: 2000,
-        origin: 'right',
-        distance: '-100px'
-    });
-
-    sr.reveal('.navslider p', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });
-
-
-    sr.reveal('.navslider1', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });
-
-    sr.reveal('.navslider1 h2', {
-        duration: 2000,
-        origin: 'right',
-        distance: '-100px'
-    });
-
-    sr.reveal('.navslider1 p', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });
-
-    sr.reveal('.newworks', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });
-
-    sr.reveal('.artistas_inicio', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });
-
-    sr.reveal('footer', {
-        duration: 2000,
-        origin: 'top',
-        distance: '-100px'
-    });
-
+//contacto
+sr.reveal('.contacto', {
+    duration: 2000,
+    origin: 'top',
+    distance: '-100px'
+});
 
